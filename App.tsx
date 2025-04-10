8b5f37d39bb08637a9fcf0a9534ab13704e226d9
mona <mona.halleem@gmail.com>
2025-04-10 22:32:06 +0200

    adding theme and fonts Imports

--- App.tsx
+++ App.tsx
@@ -1,7 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+import {
+  Inter_500Medium,
+  Inter_700Bold,
+  Inter_300Light,
+} from "@expo-google-fonts/inter";
+import {
+  Poppins_700Bold,
+  Poppins_600SemiBold,
+  Poppins_500Medium,
+  Poppins_400Regular,
+} from "@expo-google-fonts/poppins";
+import {
+  LeagueSpartan_300Light,
+  LeagueSpartan_400Regular,
+} from "@expo-google-fonts/league-spartan";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_300Light,
+    Inter_500Medium,
+    Inter_700Bold,
+
+    Poppins_700Bold,
+    Poppins_600SemiBold,
+    Poppins_500Medium,
+    Poppins_400Regular,
+
+    LeagueSpartan_300Light,
+    LeagueSpartan_400Regular,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
