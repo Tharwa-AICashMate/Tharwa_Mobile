b2ea9007f20bff2bfd2addab171bda2f86e6a432
mona <mona.halleem@gmail.com>
2025-04-29 17:54:08 +0300

    commit before Merge

--- frontend/src/screens/AddExpense/index.tsx
+++ frontend/src/screens/AddExpense/index.tsx
@@ -1,7 +1,15 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { View, SafeAreaView } from "react-native";
-import { useNavigation, useFocusEffect, useRoute, RouteProp } from "@react-navigation/native";
-import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
+import {
+  useNavigation,
+  useFocusEffect,
+  useRoute,
+  RouteProp,
+} from "@react-navigation/native";
+import {
+  NativeStackNavigationProp,
+  NativeStackScreenProps,
+} from "@react-navigation/native-stack";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 import Header from "@/componenets/HeaderIconsWithTitle/HeadericonsWithTitle";
@@ -14,19 +22,24 @@ import { editTransactionsAsync } from "@/redux/slices/transactionSlice";
 import { fetchUserCategories } from "@/redux/slices/categoriesSlice";
 
 type AddExpensesScreenNavigationProp = NativeStackNavigationProp<{
-  CategoryDetail: { categoryName: string; categoryId: number; UserId: string; Icon: string };
+  CategoryDetail: {
+    categoryName: string;
+    categoryId: number;
+    UserId: string;
+    Icon: string;
+  };
   AddExpensesScreen: undefined;
 }>;
-type editRouteProp = RouteProp<RootStackParamList, 'AddExpensesScreen'>;
+type editRouteProp = RouteProp<RootStackParamList, "AddExpensesScreen">;
 
 const AddExpensesScreen = () => {
   const navigation = useNavigation<AddExpensesScreenNavigationProp>();
   const dispatch = useAppDispatch();
   const { items: categories } = useAppSelector((state) => state.categories);
   const route = useRoute<editRouteProp>();
-  
+
   //const [userId, setUserId] = useState<string | null>(null);
-  const userId = useAppSelector(state => state.auth.user?.id);
+  const userId = useAppSelector((state) => state.auth.user?.id);
   const data = route.params;
   const transaction = data?.transaction;
   // useFocusEffect(
@@ -44,11 +57,9 @@ const AddExpensesScreen = () => {
   //   }, [])
   // );
 
-  useEffect(()=>{
-    if(!categories.length && userId)
-      dispatch(fetchUserCategories(userId))
-
-  },[userId])
+  useEffect(() => {
+    if (!categories.length && userId) dispatch(fetchUserCategories(userId));
+  }, [userId]);
 
   const handleSubmit = (data: {
     category: string;
@@ -56,10 +67,12 @@ const AddExpensesScreen = () => {
     title: string;
     type: "expence";
     message: string;
-    created_at:Date;
+    created_at: Date;
   }) => {
-    const selectedCategory = categories.find((cat) => cat.name === data.category);
-   
+    const selectedCategory = categories.find(
+      (cat) => cat.name === data.category
+    );
+
     if (!selectedCategory || !userId) {
       console.error("Category or user not found");
       return;
@@ -70,16 +83,20 @@ const AddExpensesScreen = () => {
       amount: parseFloat(data.amount),
       type: data.type,
       title: data.title || data.category,
-      created_at:data.created_at
+      created_at: data.created_at,
     };
-    if(transaction?.transaction_id){
-      dispatch(editTransactionsAsync({...newTransaction,id:transaction.transaction_id})).then((res) =>{
-        if(res.meta.requestStatus === "fulfilled"){
-          navigation.goBack()
+    if (transaction?.transaction_id) {
+      dispatch(
+        editTransactionsAsync({
+          ...newTransaction,
+          id: transaction.transaction_id,
+        })
+      ).then((res) => {
+        if (res.meta.requestStatus === "fulfilled") {
+          navigation.goBack();
         }
-      })
-    }
-    else
+      });
+    } else
       dispatch(createTransaction(newTransaction)).then((res) => {
         if (res.meta.requestStatus === "fulfilled") {
           navigation.replace("CategoryDetail", {
@@ -96,23 +113,26 @@ const AddExpensesScreen = () => {
     <SafeAreaView style={{ flex: 1, backgroundColor: Theme.colors.primary }}>
       <Header title="Add Expenses" />
       <View style={{ flex: 1, backgroundColor: Theme.colors.background }}>
-        {transaction ?
-        <TransactionForm
-          title="Expense"
-          buttonText="Save"
-          categories={categories}
-          onSubmit={handleSubmit}
-          initialCategory={transaction?.category_name}
-          initialAmount={transaction?.amount.toString()}
-          initialTitle={transaction?.title}
-          initialMessage={transaction?.description}
-          initialDate={new Date(transaction?.created_at) }
-        />:<TransactionForm
-        title="Expense"
-        buttonText="Save"
-        categories={categories}
-        onSubmit={handleSubmit}
-         />}
+        {transaction ? (
+          <TransactionForm
+            title="Expense"
+            buttonText="Save"
+            categories={categories}
+            onSubmit={handleSubmit}
+            initialCategory={transaction?.category_name}
+            initialAmount={transaction?.amount.toString()}
+            initialTitle={transaction?.title}
+            initialMessage={transaction?.description}
+            initialDate={new Date(transaction?.created_at)}
+          />
+        ) : (
+          <TransactionForm
+            title="Expense"
+            buttonText="Save"
+            categories={categories}
+            onSubmit={handleSubmit}
+          />
+        )}
       </View>
     </SafeAreaView>
   );
